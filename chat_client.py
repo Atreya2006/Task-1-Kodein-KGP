26d06337ea454d31ac94af750cb7417398960187
Atreya2006 <163442802+Atreya2006@users.noreply.github.com>
2025-06-24 17:29:34 +0530

    Update chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -1,99 +1,51 @@
+# chat_client.py
 import socket
 import threading
-import random
-import math
+import json
+from rsa import encrypt_message
 
-# RSA functions (same as provided)
-def is_prime(number):
-    if number < 2:
-        return False
-    #By definition 1 is not a prime number
-    for i in range(2, int(math.sqrt(number)) + 1): #checking if a divisor exists only till root of number to reduce time complexity
-        if number % i == 0:
-            return False
-        #If no divisors exists then number is prime and so return true
-    return True
-
-#Function to generate prime numbers between min and max values
-def generate_prime(min_value, max_value):
-    prime = random.randint(min_value, max_value)
-    #Randomly generating numbers 
-    while not is_prime(prime): #Keep generating untill the generated number is not prime
-        prime = random.randint(min_value, max_value)
-    return prime
-
-def mod_inverse(e, phi): #Here, e is the public key (known by others), phi is the value of Euler's Totient Function(not known by others)
-    for d in range(3, phi):
-        if (d * e) % phi == 1:
-            return d
-        #d is the Private key generated for the passed value of the public key e
-    raise ValueError("Mod_inverse does not exist!")
-
-def generate_rsa_keys(min_value=1000, max_value=50000):
-    p, q = generate_prime(min_value, max_value), generate_prime(min_value, max_value)
-    while p == q:
-        q = generate_prime(min_value, max_value)
-        #Keep generating p and q untill you get 2 non equal prime numbers
-    n = p * q
-    phi_n = (p - 1) * (q - 1) #Value of the Euler Totient Function
-
-    e = random.randint(3, phi_n - 1)
-    while math.gcd(e, phi_n) != 1:
-        e = random.randint(3, phi_n - 1)
-        #Finding a Public Key for the given phi value
-    d = mod_inverse(e, phi_n)
-
-    return (e, n), (d, n)  # public key, private key
-
-#Function to encrypt the messages using the Public Key and RSA Algo
-def encrypt_message(public_key, message):
-    e, n = public_key
-    encrypted_message = [pow(ord(char), e, n) for char in message]
-    return encrypted_message
-
-#Function decrypt the messages using the Private Key and RSA Algo
-def decrypt_message(private_key, encrypted_message):
-    d, n = private_key
-    decrypted_message = ''.join([chr(pow(char, d, n)) for char in encrypted_message])
-    return decrypted_message
-
-
-# Chatroom client
 class ChatClient:
     def __init__(self, host='127.0.0.1', port=12345):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP connection
-        self.client.connect((host, port))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
         self.public_key = None
-        self.private_key = None
 
-    def send_message(self, message):
-        encrypted_message = encrypt_message(self.public_key, message)
-        self.client.send(str(encrypted_message).encode())
-
-    def receive_messages(self): #Receiving messages using this function
+    def receive_loop(self):
         while True:
             try:
-                message = self.client.recv(1024)
-                if "Your public key:" in message.decode():
-                    self.public_key = eval(message.decode().split(": ")[1])
-                elif "Your private key:" in message.decode():
-                    self.private_key = eval(message.decode().split(": ")[1])
-                else:
-                    print(message.decode())
+                data = self.sock.recv(4096).decode()
+                # try JSON parse
+                try:
+                    obj = json.loads(data)
+                    if "public_key" in obj:
+                        self.public_key = tuple(obj["public_key"])
+                        print(f"[System] Received server public key: {self.public_key}")
+                        continue
+                except json.JSONDecodeError:
+                    pass
+                # fallback: plaintext broadcast
+                print(data)
             except:
-                self.client.close()
+                print("[System] Disconnected.")
                 break
 
-    def start(self, name):
-        self.client.send(name.encode())
-        threading.Thread(target=self.receive_messages).start() #starting the thread
+    def send_loop(self):
         while True:
-            message = input()
-            self.send_message(message)
+            msg = input()
+            if not self.public_key:
+                print("[!] Waiting for server public key...")
+                continue
+            encrypted = encrypt_message(self.public_key, msg)
+            packet = json.dumps({"message": encrypted}).encode()
+            self.sock.send(packet)
 
-# Run the client
+    def start(self, name):
+        # send raw name
+        self.sock.send(name.encode())
+        threading.Thread(target=self.receive_loop, daemon=True).start()
+        self.send_loop()
+
 if __name__ == "__main__":
-    name = input("Enter your name: ") #name for the server host to know who is writing what
+    name = input("Enter your name: ").strip()
     client = ChatClient()
     client.start(name)
-    #code crashes for mutliple clients , only supported communication between 2 clients and 1 server where the messages are seen in decrypted form
